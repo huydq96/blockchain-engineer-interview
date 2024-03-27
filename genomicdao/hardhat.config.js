910e44d300c7b4e1ca0118ee34174db5911b6fca
huydq96 <huydq96hp@gmail.com>
2024-03-27 17:40:18 +0700

    deploy to optimism sepolia

--- genomicdao/hardhat.config.js
+++ genomicdao/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    sepolia_test: {
+      url: `https://optimism-sepolia.infura.io/v3/${process.env.API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
 };
